756e16ab9ea16a57455750bfb6361c76323a2192
Jordan Miller <Jordan.kay@gmail.com>
2024-09-25 12:14:01 -0600

    Update wallet.py

--- satorilib/api/wallet/wallet.py
+++ satorilib/api/wallet/wallet.py
@@ -170,10 +170,7 @@ class Wallet():
     '''
 
     def authPayload(self, asDict: bool = False, challenge: str = None) -> Union[str, dict]:
-        print('before payload')
         payload = connection.authPayload(self, challenge)
-        print('after payload')
-        print(payload)
         if asDict:
             return payload
         return json.dumps(payload)

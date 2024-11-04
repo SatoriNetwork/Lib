58de6a0299bd8f09e1f4fb5acdf15b275ef613d8
Jordan Miller <Jordan.kay@gmail.com>
2024-11-03 22:37:08 -0700

    fix

--- satorilib/api/wallet/wallet.py
+++ satorilib/api/wallet/wallet.py
@@ -740,7 +740,7 @@ class Wallet(WalletBase):
             if 'SATORI' in self.watchAssets:
                 for ua in self.unspentAssets:
                     logging.debug('ua', ua)
-                   if len([tx for tx in self.transactions if tx.txid == ua['tx_hash']]) == 0:
+                    if len([tx for tx in self.transactions if tx.txid == ua['tx_hash']]) == 0:
                         new_transactions = {}  # Collect new transactions here
                         new_tranaction = self.appendTransaction(ua['tx_hash'])
                         if new_tranaction is not None:

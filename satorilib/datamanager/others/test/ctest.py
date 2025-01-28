0e78864b9dde36c6c2c3201f047b20c099bc6b40
krizhnaa <krizhnaprem@gmail.com>
2025-01-28 14:43:36 +0530

    Testing guidelines

--- satorilib/datamanager/others/test/ctest.py
+++ satorilib/datamanager/others/test/ctest.py
@@ -3,20 +3,54 @@ from satorilib.datamanager.client import DataClient
 from satorilib.datamanager import Message
 import asyncio
 
+# TODO :
+# Test for endpoints
+
+# print and check the server state
+# if client state needs change
+# correct response is returned  
+
 async def main():
     client = DataClient("localhost")
-    await client.connectToServer()
     
-    # Test the connection
-    request = Message({
-        'method': 'initiate-server-connection',
-        'id': client._generateCallId(),
-        'params': {'uuid': None}
-    })
+    # request = Message({
+    #     'method': 'initiate-server-connection',
+    #     'id': client._generateCallId(),
+    #     'params': {'uuid': None}
+    # })
+
     await client.sendRequest('0.0.0.1')
     await asyncio.sleep(5)
-    await client.serverWs.send(request.to_json())
     
-    await asyncio.sleep(30)  # Keep running for 30s
-
 asyncio.run(main())
+
+
+# endpoints to test in order
+
+# 'initiate-server-connection'
+
+# 'add-available-subscription-streams'
+
+# 'add-available-publication-streams'
+
+# 'confirm-subscription'
+
+# 'send-available-subscription' : response.streamInfo
+
+# 'stream-inactive' : check if the 
+
+# 'stream-data'
+
+# to send subscription message and check if its saved in the server's database
+# await start.dataClient.passDataToServer(
+            #     start.dataServerIp, 
+            #     uuid=stream.streamId.uuid,
+            #     data=dataForServer
+            # )
+
+    # dataForServer = pd.Dataframe({
+    #         'date_time': [timestamp],
+    #         'value': [69]
+    #     })
+
+# check if the response is recieved
--- satorilib/datamanager/others/test/stest.py
+++ satorilib/datamanager/others/test/stest.py
@@ -3,7 +3,9 @@ from satorilib.datamanager.server import DataServer
 
 async def main():
     server = DataServer("localhost", 24602)
+    server.availableStreams
     await server.startServer()
     await asyncio.Future()  # Keep running
 
 asyncio.run(main())
+

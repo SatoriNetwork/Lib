9fbd948ebbf8865aa13483f5c947d4ef74f65b4a
Mathew-Steeve <mthwsteeve@gmail.com>
2025-02-09 14:53:25 +0530

    automation test

--- satorilib/datamanager/others/test/test.py
+++ satorilib/datamanager/others/test/test.py
@@ -1,45 +1,169 @@
 import asyncio
 from satorilib.datamanager import DataServer, DataClient, Message, DataServerApi
+from satoriengine.veda.engine import Engine
+import pandas as pd
+
+uuid = "23dc3133-5b3a-5b27-803e-70a07cf3c4f7"
+async def serverStartUp():
+    dataServer = DataServer('0.0.0.0')
+    await dataServer.startServer()
+    await asyncio.sleep(1)
 
 async def checkIfLocalNeuronAuthenticated():
-    dataServer = DataServer('0.0.0.0')
-    await dataServer.startServer()
-    await asyncio.sleep(5)
+    await serverStartUp()
     
     dataClient = DataClient('0.0.0.0')
     response: Message = await dataClient.isLocalNeuronClient()
     if response.status == DataServerApi.statusSuccess.value:
           print(response.senderMsg)
-    
-def ifClientsetsPubSubInfoInDataServerCorrectly():
-        # dataserver - initialize and start listening
-        # await asyncio.sleep(10)
 
-        # initialize client
-        # response = await dataClient.setPubSub(pubSubInfo)
-        # response.status == DataServerApi.statusSuccess.value:
-        #     print(response.senderMsg)
-        pass
+async def checkIfLocalEngineAuthenticated():
+    await serverStartUp()
+    
+    # await asyncio.sleep(5)
+    
+    dataClient = DataClient('0.0.0.0')
+    pubSubInfo = {"23dc3133-5b3a-5b27-803e-70a07cf3c4f7":{"publicationUuid":"23dc3133-5b3a-5b27-803e-70a07cf3c4f7"}}
+    await dataClient.setPubsubMap(pubSubInfo)
+    response: Message = await dataClient.isLocalEngineClient()
+    if response.status == DataServerApi.statusSuccess.value:
+          print(response.senderMsg)
+    
+async def ifClientsetsPubSubInfoInDataServerCorrectly():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        pubSubInfo = {"23dc3133-5b3a-5b27-803e-70a07cf3c4f7":{"publicationUuid":"23dc3133-5b3a-5b27-803e-70a07cf3c4f7"}}
+        response = await dataClient.setPubsubMap(pubSubInfo)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+
+async def ifClientgetPubSubInfoInDataServerCorrectly():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        pubSubInfo = {"23dc3133-5b3a-5b27-803e-70a07cf3c4f7":{"publicationUuid":"23dc3133-5b3a-5b27-803e-70a07cf3c4f7"}}
+        await dataClient.setPubsubMap(pubSubInfo)
+        response = await dataClient.getPubsubMap()
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.streamInfo)
+
+async def checkIfStreamActive():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.isStreamActive('0.0.0.0',uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+
+async def checkIfStreamInActive():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        await dataClient.isLocalNeuronClient()
+        pubSubInfo = {"23dc3133-5b3a-5b27-803e-70a07cf3c4f7":{"publicationUuid":"23dc3133-5b3a-5b27-803e-70a07cf3c4f7"}}
+        await dataClient.setPubsubMap(pubSubInfo)
+        response = await dataClient.streamInactive(uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
 
 async def toCheckIfDataIsInsertedIntoClient():
-    dataServer = DataServer('0.0.0.0')
-    await dataServer.startServer()
-    await asyncio.sleep(5)
+    await serverStartUp()
     
-    # uuid = some uuid in your database
-
-    # df = pd.DataFrame({
-    #     'date_time': ['2024-10-02 04:30:06.341020'],
-    #     'value': [969.717144]
-    #     })
-    # df['date_time'] = pd.to_datetime(df['date_time'])
-    # df.set_index('date_time', inplace=True)
+    df = pd.DataFrame({
+        'value': [969.717144],
+        'hash':['bsjnlcs']
+    }, index=['2024-10-02 04:30:06.341021'])
 
 
     dataClient = DataClient('0.0.0.0')
-    # response: Message = await dataClient.insertStreamData()
-    response: Message = await dataClient.isLocalNeuronClient()
+    response: Message = await dataClient.insertStreamData(uuid,df)
     if response.status == DataServerApi.statusSuccess.value:
           print(response.senderMsg)
 
-asyncio.run(toCheckIfDataIsInsertedIntoClient())
+async def checkToDeleteStreamData():
+        await serverStartUp()
+
+        df = pd.DataFrame({
+        'value': [969.717144],
+        'hash':['bsjnlcs']
+    }, index=['2024-10-02 04:30:06.341021'])
+        
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.deleteStreamData(uuid,df)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+
+async def checkToGetRemoteStreamdata():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.getRemoteStreamData('0.0.0.0',uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.data)
+
+async def checkToGetLocalStreamdata():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.getLocalStreamData(uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.data)
+
+async def checkToGetAvailableSubscriptions():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.getAvailableSubscriptions('0.0.0.0')
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.streamInfo)
+
+async def checkToGetStreamDataByRange():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        fromDate='2024-12-01 07:50:00.434717'
+        toDate='2024-12-06 16:00:00.902535'
+        response = await dataClient.getStreamDataByRange('0.0.0.0',uuid,fromDate,toDate)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.data)
+
+async def checkToGetStreamObservationByTime():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        toDate='2024-12-01 07:50:00.434717'
+        response = await dataClient.getStreamObservationByTime('0.0.0.0',uuid,toDate)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+            print(response.data)
+
+async def checkToAddActiveStream():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.addActiveStream(uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+
+async def checkTo_addStreamToServer():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient._addStreamToServer(subUuid=uuid,pubUuid=uuid)
+
+async def checkToSubscribe():
+        await serverStartUp()
+
+        dataClient = DataClient('0.0.0.0')
+        response = await dataClient.subscribe('0.0.0.0',uuid,uuid)
+        if response.status == DataServerApi.statusSuccess.value:
+            print(response.senderMsg)
+
+asyncio.run(checkIfLocalNeuronAuthenticated())
+

da89df73d0d9cdbdaf6ae11679848302f7c658df
krizhnaa <krizhnaprem@gmail.com>
2025-02-21 15:56:42 +0530

    test

--- satorilib/datamanager/others/test/pubclient.py
+++ satorilib/datamanager/others/test/pubclient.py
@@ -8,16 +8,22 @@ from satorineuron import config
 
 async def main():
     df = pd.DataFrame({
-        'value': [968.717144],
+        'value': [69699.717144],
         'hash':['bsjnlct']
-    }, index=['2024-10-03 04:30:06.341021'])
+    }, index=['2025-10-06 04:30:06.341021'])
 
     walletPath = config.walletPath('wallet.yaml')
-    dataClient = DataClient('0.0.0.0', identity=EvrmoreIdentity(walletPath))
+    dataClient = DataClient('159.65.144.150', identity=EvrmoreIdentity(walletPath))
+    response: Message = await dataClient.authenticate(islocal='neuron')
+    response: Message = await dataClient.addActiveStream('009bb819-b737-55f5-b4d7-d851316eceae')
+    # response: Message = await dataClient.isStreamActive('159.65.144.150', '009bb819-b737-55f5-b4d7-d851316eceae')
     response: Message = await dataClient.insertStreamData('009bb819-b737-55f5-b4d7-d851316eceae', 
                                                           df, 
                                                           isSub=True)
-    if response.status == DataServerApi.statusSuccess.value:
-          print(response.senderMsg)
+    # response: Message = await dataClient.isStreamActive('159.65.144.150', '009bb819-b737-55f5-b4d7-d851316eceae')
+    # print(response.to_dict(True))
+    # if response.status == DataServerApi.statusSuccess.value:
+    print(response.senderMsg)
+    await asyncio.Event().wait()
 
 asyncio.run(main())
